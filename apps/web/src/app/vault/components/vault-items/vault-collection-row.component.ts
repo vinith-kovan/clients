e19ccffc8e57a47b78d64af77677c76540fa34b7
jng <jng@bitwarden.com>
2023-10-31 13:17:09 -0400

    update text in admin console collections

--- apps/web/src/app/vault/components/vault-items/vault-collection-row.component.ts
+++ apps/web/src/app/vault/components/vault-items/vault-collection-row.component.ts
@@ -52,6 +52,8 @@ export class VaultCollectionRowComponent implements OnInit {
       this.permissionText = "canManage";
     } else if (!this.collection.readOnly) {
       this.permissionText = "canEdit";
+    } else if (this.collection.readOnly) {
+      this.permissionText = "canView";
     } else {
       this.permissionText = null;
     }

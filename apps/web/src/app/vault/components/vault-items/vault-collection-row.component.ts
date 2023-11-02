6f2efe20293889f648924be53d6856407f2c2e66
jng <jng@bitwarden.com>
2023-11-02 15:13:10 -0400

    fix conflicts

--- apps/web/src/app/vault/components/vault-items/vault-collection-row.component.ts
+++ apps/web/src/app/vault/components/vault-items/vault-collection-row.component.ts
@@ -9,7 +9,6 @@ import {
 } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
-import { OrganizationUserType } from "@bitwarden/common/admin-console/enums";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { CollectionView } from "@bitwarden/common/vault/models/view/collection.view";
 
@@ -36,6 +35,7 @@ export class VaultCollectionRowComponent implements OnInit {
   @Input() organizations: Organization[];
   @Input() groups: GroupView[];
   @Input() orgVault: boolean;
+  @Input() isOrgOwner: boolean;
   permissionText: string;
 
   @Output() onEvent = new EventEmitter<VaultItemEvent>();
@@ -49,16 +49,12 @@ export class VaultCollectionRowComponent implements OnInit {
   // https://github.com/bitwarden/clients/pull/6417
 
   ngOnInit() {
-    // change this to use new collection data
-    if (
-      this.organizations[0].type === OrganizationUserType.Owner ||
-      this.organizations[0].type === OrganizationUserType.Manager
-    ) {
-      this.permissionText = "canManage"; // how to bring this from json. maybe set the json key
-    } else if (this.organizations[0].type === OrganizationUserType.Admin) {
+    if (this.collection.manage || this.isOrgOwner) {
+      this.permissionText = "canManage";
+    } else if (!this.collection.readOnly) {
       this.permissionText = "canEdit";
     } else {
-      this.permissionText = "-";
+      this.permissionText = null;
     }
   }
 
--- apps/web/src/app/vault/org-vault/vault.component.ts
+++ apps/web/src/app/vault/org-vault/vault.component.ts
@@ -49,6 +49,7 @@ import { MessagingService } from "@bitwarden/common/platform/abstractions/messag
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
+import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
 import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 import { CipherRepromptType } from "@bitwarden/common/vault/enums/cipher-reprompt-type";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
@@ -163,6 +164,7 @@ export class VaultComponent implements OnInit, OnDestroy {
     private eventCollectionService: EventCollectionService,
     private totpService: TotpService,
     private apiService: ApiService,
+    private collectionService: CollectionService,
     protected configService: ConfigServiceAbstraction
   ) {}
 
@@ -231,8 +233,23 @@ export class VaultComponent implements OnInit, OnDestroy {
 
     this.currentSearchText$ = this.route.queryParams.pipe(map((queryParams) => queryParams.search));
 
-    const allCollectionsWithoutUnassigned$ = organizationId$.pipe(
-      switchMap((orgId) => this.collectionAdminService.getAll(orgId)),
+    const allCollectionsWithoutUnassigned$ = combineLatest([
+      organizationId$.pipe(switchMap((orgId) => this.collectionAdminService.getAll(orgId))),
+      this.collectionService.getAllDecrypted(),
+    ]).pipe(
+      map(([adminCollections, syncCollections]) => {
+        adminCollections.forEach((collection) => {
+          const currentId = collection.id;
+          const match = syncCollections.find((syncCollection) => currentId === syncCollection.id);
+
+          if (match) {
+            collection.manage = match.manage;
+            collection.readOnly = match.readOnly;
+          }
+          return collection;
+        });
+        return adminCollections;
+      }),
       shareReplay({ refCount: true, bufferSize: 1 })
     );
 
@@ -453,7 +470,6 @@ export class VaultComponent implements OnInit, OnDestroy {
           this.collections = collections;
           this.selectedCollection = selectedCollection;
           this.showMissingCollectionPermissionMessage = showMissingCollectionPermissionMessage;
-
           this.isEmpty = collections?.length === 0 && ciphers?.length === 0;
 
           // This is a temporary fix to avoid double fetching collections.

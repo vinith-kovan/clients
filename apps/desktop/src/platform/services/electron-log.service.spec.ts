ed4cb06433b4371fe68373ef38fd73476eb42251
Daniel García <dani-garcia@users.noreply.github.com>
2023-11-28 13:40:44 +0100

    Mock electron API

--- apps/desktop/src/platform/services/electron-log.service.spec.ts
+++ apps/desktop/src/platform/services/electron-log.service.spec.ts
@@ -1,5 +1,10 @@
 import { ElectronLogMainService } from "./electron-log.main.service";
 
+// Mock the use of the electron API to avoid errors
+jest.mock("electron", () => ({
+  ipcMain: { handle: jest.fn() },
+}));
+
 describe("ElectronLogMainService", () => {
   it("sets dev based on electron method", () => {
     process.env.ELECTRON_IS_DEV = "1";

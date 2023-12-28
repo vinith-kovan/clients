ac037097a025d620f4b8164f1841f712d818e4c4
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-12-28 18:00:58 +0100

    Fix vault-export-service tests not running

--- libs/tools/export/vault-export/vault-export-core/jest.config.js
+++ libs/tools/export/vault-export/vault-export-core/jest.config.js
@@ -2,14 +2,12 @@ const { pathsToModuleNameMapper } = require("ts-jest");
 
 const { compilerOptions } = require("../../../../shared/tsconfig.libs");
 
-const sharedConfig = require("../../../../shared/jest.config.ts");
-
 /** @type {import('jest').Config} */
 module.exports = {
-  ...sharedConfig,
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths || {}, {
-    prefix: "<rootDir>/",
+    prefix: "<rootDir>/../../../",
   }),
 };

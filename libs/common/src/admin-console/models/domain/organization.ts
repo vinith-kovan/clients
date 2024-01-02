ae2f9fbf96078d4d5a995429f71d02b358f9fbec
Thomas Rittson <trittson@bitwarden.com>
2024-01-02 14:04:56 +1000

    Add flexibleCollections property to sync data

--- libs/common/src/admin-console/models/domain/organization.ts
+++ libs/common/src/admin-console/models/domain/organization.ts
@@ -72,6 +72,10 @@ export class Organization {
    * Refers to the ability for an owner/admin to access all collection items, regardless of assigned collections
    */
   allowAdminAccessToAllCollectionItems: boolean;
+  /**
+   * Returns true if this organization has enabled Flexible Collections (MVP) and their data has been migrated.
+   * Generally, you should use this as the feature flag to gate Flexible Collections features.
+   */
   flexibleCollections: boolean;
 
   constructor(obj?: OrganizationData) {
@@ -126,6 +130,7 @@ export class Organization {
     this.accessSecretsManager = obj.accessSecretsManager;
     this.limitCollectionCreationDeletion = obj.limitCollectionCreationDeletion;
     this.allowAdminAccessToAllCollectionItems = obj.allowAdminAccessToAllCollectionItems;
+    this.flexibleCollections = obj.flexibleCollections;
   }
 
   get canAccess() {

ffa991a8f0d38e96954991457ca6ef0edaaad1bc
gbubemismith <gsmithwalter@gmail.com>
2024-01-05 12:01:35 -0500

    changed chalk import

--- apps/cli/src/program.ts
+++ apps/cli/src/program.ts
@@ -1,4 +1,4 @@
-import * as chalk from "chalk";
+import chalk from "chalk";
 import * as program from "commander";
 
 import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
--- apps/cli/src/tools/send/send.program.ts
+++ apps/cli/src/tools/send/send.program.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import * as chalk from "chalk";
+import chalk from "chalk";
 import * as program from "commander";
 
 import { Utils } from "@bitwarden/common/platform/misc/utils";

8eb74f259f5e9336d0763ea2e2a9f46f14a4ae88
Jared Snider <jsnider@bitwarden.com>
2024-01-08 17:19:35 -0500

    PM-5242 - Update libs/auth angular index to take renamed component into account

--- libs/auth/src/angular/index.ts
+++ libs/auth/src/angular/index.ts
@@ -6,4 +6,4 @@ export * from "./password-callout/password-callout.component";
 
 // user verification
 export * from "./user-verification/user-verification-dialog.component";
-export * from "./user-verification/user-verification-form.component";
+export * from "./user-verification/user-verification-form-input.component";

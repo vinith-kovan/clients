7b59af0890d8ed3ae48729ed9be001d38c6902c5
Jared Snider <jsnider@bitwarden.com>
2024-01-08 19:18:13 -0500

    PM-5242 - UserVerificationFormInput component - WIP draft of new client and server split of user verification logic

--- libs/auth/src/angular/user-verification/user-verification-form-input.component.ts
+++ libs/auth/src/angular/user-verification/user-verification-form-input.component.ts
@@ -11,12 +11,28 @@ import {
 import { Subject, takeUntil } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { VaultTimeoutSettingsService } from "@bitwarden/common/abstractions/vault-timeout/vault-timeout-settings.service";
 import { UserVerificationService } from "@bitwarden/common/auth/abstractions/user-verification/user-verification.service.abstraction";
 import { VerificationType } from "@bitwarden/common/auth/enums/verification-type";
 import { Verification } from "@bitwarden/common/auth/types/verification";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { AsyncActionsModule, FormFieldModule, IconButtonModule } from "@bitwarden/components";
+
+type UserVerificationOptions = {
+  server?: {
+    otp: boolean;
+    masterPassword: boolean;
+  };
+  client?: {
+    masterPassword: boolean;
+    pin: boolean;
+    biometrics: boolean;
+  };
+};
+
+type ActiveClientVerificationOption = "masterPassword" | "pin" | "biometrics" | "none";
 
 /**
  * Used for general-purpose user verification throughout the app.
@@ -51,6 +67,13 @@ import { AsyncActionsModule, FormFieldModule, IconButtonModule } from "@bitwarde
 })
 // eslint-disable-next-line rxjs-angular/prefer-takeuntil
 export class UserVerificationFormInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
+  @Input() verificationType: "server" | "client";
+
+  // This represents what verification methods are available to the user.
+  userVerificationOptions: UserVerificationOptions;
+
+  activeClientVerificationOption: ActiveClientVerificationOption;
+
   private _invalidSecret = false;
   @Input()
   get invalidSecret() {
@@ -71,7 +94,6 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
   }
   @Output() invalidSecretChange = new EventEmitter<boolean>();
 
-  hasMasterPassword = true;
   disableRequestOTP = false;
   sentCode = false;
 
@@ -81,7 +103,7 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
       if (this.invalidSecret) {
         return {
           invalidSecret: {
-            message: this.hasMasterPassword
+            message: this.userVerificationOptions.client.masterPassword
               ? this.i18nService.t("incorrectPassword")
               : this.i18nService.t("incorrectCode"),
           },
@@ -96,10 +118,13 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
   constructor(
     private userVerificationService: UserVerificationService,
     private i18nService: I18nService,
+    private vaultTimeoutSettingsService: VaultTimeoutSettingsService,
+    private platformUtilsService: PlatformUtilsService,
   ) {}
 
   async ngOnInit() {
-    this.hasMasterPassword = await this.userVerificationService.hasMasterPasswordAndMasterKeyHash();
+    await this.determineAvailableVerificationMethods();
+
     this.processChanges(this.secret.value);
 
     this.secret.valueChanges
@@ -107,8 +132,53 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
       .subscribe((secret: string) => this.processChanges(secret));
   }
 
+  private async determineAvailableVerificationMethods(): Promise<void> {
+    if (this.verificationType === "client") {
+      const [userHasMasterPassword, pinLockType, biometricsEnabled] = await Promise.all([
+        this.userVerificationService.hasMasterPasswordAndMasterKeyHash(),
+        this.vaultTimeoutSettingsService.isPinLockSet(),
+        this.platformUtilsService.supportsBiometric(),
+      ]);
+
+      this.userVerificationOptions.client = {
+        masterPassword: userHasMasterPassword,
+        pin: pinLockType !== "DISABLED",
+        biometrics: biometricsEnabled,
+      };
+
+      this.setActiveClientVerificationOption();
+    } else {
+      const userHasMasterPassword =
+        await this.userVerificationService.hasMasterPasswordAndMasterKeyHash();
+
+      this.userVerificationOptions.server = {
+        masterPassword: userHasMasterPassword,
+        otp: !userHasMasterPassword,
+      };
+    }
+  }
+
+  private setActiveClientVerificationOption(): void {
+    // TODO: do we have priorities for which of these should be the default if multiple are enabled?
+
+    const clientVerificationOptions: (keyof typeof this.userVerificationOptions.client)[] = [
+      "masterPassword",
+      "pin",
+      "biometrics",
+    ];
+    this.activeClientVerificationOption =
+      (clientVerificationOptions.find(
+        (method) => this.userVerificationOptions.client[method],
+      ) as ActiveClientVerificationOption) || "none";
+
+    // TODO: whenever we change the active verification option, we need to clear the secret field
+    // and if the user chose biometrics, we need to prompt them for biometrics
+    // Might be worth wrapping the setting of the active verification option in an behavior subject
+    // or maybe a setter that does this for us
+  }
+
   requestOTP = async () => {
-    if (!this.hasMasterPassword) {
+    if (!this.userVerificationOptions.client.masterPassword) {
       this.disableRequestOTP = true;
       try {
         await this.userVerificationService.requestOTP();
@@ -123,10 +193,12 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
     this.secret.setValue(obj);
   }
 
+  /** Required for NG_VALUE_ACCESSOR */
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
+  /** Required for NG_VALUE_ACCESSOR */
   registerOnTouched(fn: any): void {
     // Not implemented
   }
@@ -140,12 +212,7 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
     }
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  protected processChanges(secret: string) {
+  processChanges(secret: string) {
     this.invalidSecret = false;
 
     if (this.onChange == null) {
@@ -153,8 +220,15 @@ export class UserVerificationFormInputComponent implements ControlValueAccessor,
     }
 
     this.onChange({
-      type: this.hasMasterPassword ? VerificationType.MasterPassword : VerificationType.OTP,
+      type: this.userVerificationOptions.client.masterPassword
+        ? VerificationType.MasterPassword
+        : VerificationType.OTP,
       secret: Utils.isNullOrWhitespace(secret) ? null : secret,
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
